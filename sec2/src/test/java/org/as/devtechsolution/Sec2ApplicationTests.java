5ea30cc417f099df57327a0bc33d554da706e5f8
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2025-06-08 17:57:03 +0530

    Initial Commit

---
+++ sec2/src/test/java/org/as/devtechsolution/Sec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
