4981a978f3cddcb5572a06ed63da87e6e507821e
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2025-06-08 20:56:54 +0530

    Initial Commit

---
+++ sec3/src/main/java/org/as/devtechsolution/Sec3Application.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sec3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sec3Application.class, args);
+    }
+
+}
