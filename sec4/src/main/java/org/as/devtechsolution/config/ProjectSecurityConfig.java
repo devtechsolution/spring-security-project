970c4fc32856a18618a8a89944917f039dfe0210
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2025-06-09 14:46:45 +0530

    Default JDBC User Implementation

--- sec4/src/main/java/org/as/devtechsolution/config/ProjectSecurityConfig.java
+++ sec4/src/main/java/org/as/devtechsolution/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package org.as.devtechsolution.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -10,7 +11,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -37,17 +42,17 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}12345").authorities("Read").build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$3h6oCwwZHCq7L9P4Pwr4D.qGsj3GLmVWpXBo5eGoTgHz6CCWZ4vMG").authorities("admin").build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
